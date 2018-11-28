5febd5445131850ec9615ffc4ef21ae900476867
notskm <41784014+notskm@users.noreply.github.com>
2018-11-28 03:14:23 -0500

    Create a window

--- src/snake/main.cpp
+++ src/snake/main.cpp
@@ -4,6 +4,30 @@
 	https://opensource.org/licenses/BSD-3-Clause
 */
 
+#include <SFML/Graphics.hpp>
+
 int main()
 {
+	sf::RenderWindow window{{640, 480}, "Snake", sf::Style::Close};
+
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			switch (event.type) {
+			case sf::Event::Closed:
+				window.close();
+				break;
+			case sf::Event::KeyReleased:
+				if (event.key.code == sf::Keyboard::Escape) {
+					window.close();
+				}
+				break;
+			default:
+				break;
+			}
+		}
+
+		window.clear();
+		window.display();
+	}
 }
