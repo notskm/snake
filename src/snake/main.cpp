1d3b4c4e8289c4fbb6091e2f241d574a340f82cf
notskm <41784014+notskm@users.noreply.github.com>
2018-11-29 02:23:31 -0500

    Define the cell size and create an apple

--- src/snake/main.cpp
+++ src/snake/main.cpp
@@ -5,11 +5,21 @@
 */
 
 #include <SFML/Graphics.hpp>
+#include <cassert>
+#include <gsl/gsl>
+
+constexpr float cell_size{8.f};
 
 int main()
 {
 	sf::RenderWindow window{{640, 480}, "Snake", sf::Style::Close};
 	window.setVerticalSyncEnabled(true);
+
+	assert(window.getSize().x % gsl::narrow<int>(cell_size) == 0);
+	assert(window.getSize().y % gsl::narrow<int>(cell_size) == 0);
+
+	sf::RectangleShape apple{{cell_size, cell_size}};
+	apple.setFillColor(sf::Color::Red);
 
 	while (window.isOpen()) {
 		sf::Event event;
@@ -29,6 +39,7 @@ int main()
 		}
 
 		window.clear();
+		window.draw(apple);
 		window.display();
 	}
 }
