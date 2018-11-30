465023ba9cbe2e31575f97ba320970c13b8381ba
notskm <41784014+notskm@users.noreply.github.com>
2018-11-30 03:49:53 -0500

    Simplify update rate limit
    There is no need to use anything other than
    sf::RenderWindow::setFramerateLimit to limit the update rate. Any other
    solution adds complexity that isn't required for a game this simple.

--- src/snake/main.cpp
+++ src/snake/main.cpp
@@ -55,7 +55,7 @@ void append_snake_unit(std::list<sf::RectangleShape>& snake)
 int main()
 {
 	sf::RenderWindow window{{640, 480}, "Snake", sf::Style::Close};
-	window.setVerticalSyncEnabled(true);
+	window.setFramerateLimit(15);
 
 	assert(cell_size_int > 0);
 	assert(cell_size_int == cell_size_real);
@@ -71,10 +71,6 @@ int main()
 	append_snake_unit(snake);
 
 	direction snake_move_direction{direction::right};
-
-	sf::Clock clock;
-	sf::Time time_since_update{sf::Time::Zero};
-	const sf::Time tick{sf::seconds(1.f / 15.f)};
 
 	while (window.isOpen()) {
 		sf::Event event;
@@ -93,57 +89,51 @@ int main()
 			}
 		}
 
-		time_since_update += clock.restart();
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+			if (snake_move_direction != direction::down) {
+				snake_move_direction = direction::up;
+			}
+		}
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+			if (snake_move_direction != direction::up) {
+				snake_move_direction = direction::down;
+			}
+		}
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+			if (snake_move_direction != direction::right) {
+				snake_move_direction = direction::left;
+			}
+		}
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+			if (snake_move_direction != direction::left) {
+				snake_move_direction = direction::right;
+			}
+		}
 
-		if (time_since_update >= tick) {
-			time_since_update = sf::Time::Zero;
+		snake.back().setPosition(snake.front().getPosition());
+		snake.splice(snake.begin(), snake, std::prev(snake.end()));
 
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-				if (snake_move_direction != direction::down) {
-					snake_move_direction = direction::up;
-				}
-			}
-			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
-				if (snake_move_direction != direction::up) {
-					snake_move_direction = direction::down;
-				}
-			}
-			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-				if (snake_move_direction != direction::right) {
-					snake_move_direction = direction::left;
-				}
-			}
-			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-				if (snake_move_direction != direction::left) {
-					snake_move_direction = direction::right;
-				}
-			}
+		switch (snake_move_direction) {
+		case direction::up:
+			snake.front().move(0.f, -cell_size_real);
+			break;
+		case direction::down:
+			snake.front().move(0.f, cell_size_real);
+			break;
+		case direction::left:
+			snake.front().move(-cell_size_real, 0.f);
+			break;
+		case direction::right:
+			snake.front().move(cell_size_real, 0.f);
+			break;
+		default:
+			break;
+		}
 
+		if (snake.front().getPosition() == apple.getPosition()) {
+			append_snake_unit(snake);
 			snake.back().setPosition(snake.front().getPosition());
-			snake.splice(snake.begin(), snake, std::prev(snake.end()));
-
-			switch (snake_move_direction) {
-			case direction::up:
-				snake.front().move(0.f, -cell_size_real);
-				break;
-			case direction::down:
-				snake.front().move(0.f, cell_size_real);
-				break;
-			case direction::left:
-				snake.front().move(-cell_size_real, 0.f);
-				break;
-			case direction::right:
-				snake.front().move(cell_size_real, 0.f);
-				break;
-			default:
-				break;
-			}
-
-			if (snake.front().getPosition() == apple.getPosition()) {
-				append_snake_unit(snake);
-				snake.back().setPosition(snake.front().getPosition());
-				apple.setPosition(random_cell_position(window.getSize()));
-			}
+			apple.setPosition(random_cell_position(window.getSize()));
 		}
 
 		window.clear();
